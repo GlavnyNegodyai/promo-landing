4ace64ebf9dd8ef2f1a7adfc777256229f86f8df
Pavel(aka GlavGad) <glavnynegodyai@gmail.com>
2026-08-12 10:09:57 +0700

    Изменения для деплоя через Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,18 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://glavnynegodyai.github.io',
+  base: '/promo-landing',
   integrations: [
     react({
       strictMode: false,
     }),
   ],
+
+  server: {
+    host: '127.0.0.1',
+    port: 4321,
+  },
 
   vite: {
     plugins: [tailwindcss()],
